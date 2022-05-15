367cddd88f22dbb0361ca087980e8a8be138128a
Laurent <ldmckkb@gmail.com>
2022-05-15 07:28:50 -0500

    paths updated

--- models/routesDAO.js
+++ models/routesDAO.js
@@ -1,12 +1,15 @@
 const fs = require('fs')
 
-const databasePath = {
+const paths = {
   win32: 'C:/SIAM-Data/routes-data.json',
-  darwin: '/home/SIAM/routes-data.json'
+  darwin: '~/SIAM/routes-data.json',
+  linux: '~/SIAM/routes-data.json'
 }
 
+const databasePath = paths[process.platform]
+
 const getData = () => {
-  data = fs.readFileSync(databasePath[process.platform]).toString()
+  data = fs.readFileSync(databasePath).toString()
   return JSON.parse(data)
 }
 
